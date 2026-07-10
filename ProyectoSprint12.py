726993c8f68a573c6df62f4638da7aad1d059593
Memo <fretn655@gmail.com>
2026-07-10 14:16:28 -0600

    se agregan algunas lineas que sirven para imprimir ciertos datos de cada df para comprobar si no necesitan algun tipo de cambio, limpieza o correccion en generla.

--- ProyectoSprint12.py
+++ ProyectoSprint12.py
@@ -16,3 +16,25 @@ region2 = pd.read_csv(r"C:\Users\sasor\Desktop\Tripleten\Sprint 12\Proyecto\geo_
 
 """se usa la libreria pandas para pooder usar read_csv y poder tener la base de cada uno de los dfs para trabajar con ellos"""
 
+# para verificar la estructura de cada df
+region0.shape
+region1.shape
+region2.shape
+
+region0.info()
+region1.info()
+region2.info()
+
+region0.dtypes
+region1.dtypes
+region2.dtypes
+
+region0.isnull().sum()
+region1.isnull().sum()
+region2.isnull().sum()
+
+"""no se enontraron inconsistencias en cada df individial"""
+
+# 2. Entrena y prueba el modelo para cada región en geo_data_0.csv:
+    # Divide los datos en un conjunto de entrenamiento y un conjunto de validación en una proporción de 75:25
+
