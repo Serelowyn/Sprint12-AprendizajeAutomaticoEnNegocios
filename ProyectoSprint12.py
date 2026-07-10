6cdd13dddfae5012a8d9345afaa533421bcdf46e
Memo <fretn655@gmail.com>
2026-07-10 14:00:54 -0600

    se agregan los 3 dataframes por region a usar.

---
+++ ProyectoSprint12.py
@@ -0,0 +1,14 @@
+# ------------------------ Importaciones
+
+import pandas as pd
+
+# ------------------------ Fin de las Importaciones
+
+#para la entrega en tripleten
+#region0 = pd.read_csv("/datasets/geo_data_0.csv")
+#region1 = pd.read_csv("/datasets/geo_data_1.csv")
+#region2 = pd.read_csv("/datasets/geo_data_2.csv")
+
+region0 = pd.read_csv(r"C:\Users\sasor\Desktop\Tripleten\Sprint 12\Proyecto\geo_data_0.csv")
+region1 = pd.read_csv(r"C:\Users\sasor\Desktop\Tripleten\Sprint 12\Proyecto\geo_data_1.csv")
+region2 = pd.read_csv(r"C:\Users\sasor\Desktop\Tripleten\Sprint 12\Proyecto\geo_data_2.csv")
